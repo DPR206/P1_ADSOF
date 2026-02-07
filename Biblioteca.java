f79764a7a96fb58653a112299fa47c30a9bff92a
DPR206 <duna.puente@estudiante.uam.es>
2026-02-07 21:48:17 +0100

    Clase biblioteca terminada

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,76 +1,74 @@
-import java.util.*;
-
 /**
- * Esta clase representa una biblioteca con una colección de libros
- * @author Duna Puente y Claudia Saiz
+ * 
+ */
+package p1;
+
+import java.util.*;
+/**
+ * Esta clase representa una biblioteca
+ * @author Claudia Saiz Escribano y Duna Puente Romera. claudia.saiz@estudiante.uam.es y duna.puente@estudiante.uam.es
  * @version 1.0
  * Nombre del fichero: Biblioteca.java
+ * 
  */
 public class Biblioteca {
-  private String nombre;
-  private List <Libro> libros = new ArrayList <>();
-
-  /**
-   * Crea una nueva biblioteca y le asigna un nombre
-   * 
-   * @param nombre nombre de la biblioteca
-   * @return nueva biblioteca creada
-   */
-  public Biblioteca(String nombre) {
-    this.nombre = nombre;
-  }
-
-  /**
-   * Añade un libro a la biblioteca
-   * 
-   * @param libro libro que se añade a la biblioteca
-   */
-  public void añadirLibro(Libro libro) {
-    libros.add(libro);
-  }
-
-  /**
-   * Obtiene una lista con todos los libros de un género dado
-   * 
-   * @param genero género literario de los libros a encontrar
-   * @return lista con los libros
-   */
-  public String librosPorGenero(String genero) {
-    String resultado = "";
-
-    for(Libro l : libros){
-      if(l.getGenero().equals(genero)){
-        resultado += l + "\n";
-      }
-    }
-    return resultado;
-  }
-
-  /**
-   * Obtiene una lista con todos los libros publicados después de un año dado
-   * 
-   * @param año año a partir del cual se encuentran los libros
-   * @return lista con los libros
-   */
-  public String librosPosterioresA(int añoPublicacion) {
-    String resultado = "";
-
-    for(Libro l : libros){
-      if(l.getAño() > añoPublicacion){
-        resultado += l + "\n";
-      }
-    }
-    return resultado;
-  }
-
-  /**
-   * Obtiene la representación de la biblioteca en forma de cadena de texto,
-   * con su nombre y la información de todos los libros que contiene
-   * 
-   * @return información de la biblioteca
-   */
-  @Override
-  public String toString(){
-    return "Nombre: " + nombre + "\nLibros: \n" + libros;
-  }
+	private String nombre;
+	private HashMap<String, List<Libro>> libros_genero = new HashMap<>();
+	private TreeMap<Integer, List<Libro>> libros_año = new TreeMap<>();
+	
+	/**
+	 * Crea una biblioteca
+	 * 
+	 * @param nombre nombre de la biblioteca
+	 * @return nueva biblioteca creada
+	 */
+	public Biblioteca(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	/**
+	 * Añade un nuevo libro a la biblioteca
+	 * 
+	 * @param nuevolibro libro a añadir
+	 */
+	public void nuevolibro(Libro nuevolibro) {
+		libros_genero.computeIfAbsent(nuevolibro.getGenero(), k -> new ArrayList<>()).add(nuevolibro);
+		libros_año.computeIfAbsent(nuevolibro.getAño(), k -> new ArrayList<>()).add(nuevolibro);
+		return;
+	}
+	
+	/**
+	 * Filtrar los libros de un género
+	 * 
+	 * @param genero género de los libros a buscar
+	 * @return lista de libros del género pedido, si el género no existe se devuelve una lista vacía
+	 */
+	public List<Libro> librosGenero(String genero) {
+		return libros_genero.getOrDefault(genero, new ArrayList<>()); 
+	}
+	
+	/**
+	 * Filtrar los libros posteriores a un año
+	 * 
+	 * @param añoPublicacion año a partir del cual se buscan los libros
+	 * @return lista de libros que se publicaron después del año solicitado
+	 */
+	public List<Libro> librosAno(int añoPublicacion) {
+		List<Libro> librosAño = new ArrayList<>();
+		SortedMap <Integer, List<Libro>> tail = libros_año.tailMap(añoPublicacion, false);
+		for(List<Libro> listaAño : tail.values())
+			librosAño.addAll(listaAño);
+		return librosAño;
+	}
+	
+	/**
+	 * Obtiene la representación de la biblioteca en forma de cadena de texto,
+	 * con su nombre y la información de todos los libros que contiene dividos según su género
+	 * 
+	 * @return información de la biblioteca
+	 */
+	@Override
+	public String toString(){
+		return "Nombre: " + nombre + "\nLibros: \n" + libros_genero;
+	}
 }
