a9286fcb33cfa93dbedd66b7d28f4cdaf95d288c
DPR206 <duna.puente@estudiante.uam.es>
2026-02-08 12:23:50 +0100

    Tests

--- TestBiblioteca_2.java
+++ TestBiblioteca_2.java
@@ -5,21 +5,18 @@
  */
 package p1;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 /**
  * Esta clase prueba las funcionalidades de la clase biblioteca
  * @author Duna Puente Romera y Claudia Saiz Escribano
  * @version 1.1
- * Nombre del fichero: TestBiblioteca_1.java
+ * Nombre del fichero: TestBiblioteca_2.java
  */
-public class TestBiblioteca_1 {
+public class TestBiblioteca_2 {
 
 	/**
 	 * Punto de entrada de la aplicación
-	 * El programa prueba a prestar, devolver y añadir libros a una biblioteca,
-	 * además de clasificar los libros según año y género
+	 * El programa prueba a prestar, devolver y añadir libros tanto con género y año como sin él
 	 * 
 	 * @param args Argumentos de la línea de comandos
 	 */
@@ -28,23 +25,24 @@ public class TestBiblioteca_1 {
 		List<Libro> libros = new ArrayList<> (List.of(
 				new Libro("1", "El Quijote", "Miguel de Cervantes", 5, 1605, "Novela"),
 				new Libro("2", "El murciélago", "Jo Nesbo", 1, 1997, "Misterio"),
-				new Libro("3", "Learn Java", "David Hoffman", 6, 2010, "Educación")));
+				new Libro("3", "Learn Java", "David Hoffman", 6, 2010, "Educación"),
+				new Libro("4", "Diez Negritos", "Agatha Christie", 7)));
 		Biblioteca biblio = new Biblioteca("BIBLIO");
 		for(Libro l :libros)
 			biblio.nuevolibro(l);
 		
-		libros.get(1).prestar();
+		libros.get(3).prestar();
 		System.out.println(biblio);
 		
-		libros.get(1).devolver();
+		libros.get(3).devolver();
 		System.out.println(biblio);
 		
-		libros.add(new Libro("4", "Con viento solano", "Ignacio Aldecoa", 1, 2007, "Novela"));
+		libros.add(new Libro("4", "Con viento solano", "Ignacio Aldecoa", 1));
 		biblio.nuevolibro(libros.getLast());
 		System.out.println(biblio);
 		
 		List<Libro> libros_año = new ArrayList<> ();
-		libros_año = biblio.librosPosterioresA(1997);
+		libros_año = biblio.librosPosterioresA(1605);
 		for(Libro l: libros_año)
 			System.out.println(l);
 		
@@ -52,6 +50,7 @@ public class TestBiblioteca_1 {
 		libros_genero = biblio.librosPorGenero("Novela");
 		for(Libro l: libros_genero)
 			System.out.println(l);
+		
 	}
 
 }
--- TestBiblioteca_3.java
+++ TestBiblioteca_3.java
@@ -5,21 +5,18 @@
  */
 package p1;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 /**
  * Esta clase prueba las funcionalidades de la clase biblioteca
  * @author Duna Puente Romera y Claudia Saiz Escribano
  * @version 1.1
- * Nombre del fichero: TestBiblioteca_1.java
+ * Nombre del fichero: TestBiblioteca_3.java
  */
-public class TestBiblioteca_1 {
+public class TestBiblioteca_3 {
 
 	/**
 	 * Punto de entrada de la aplicación
-	 * El programa prueba a prestar, devolver y añadir libros a una biblioteca,
-	 * además de clasificar los libros según año y género
+	 * El programa prueba a filtrar los libros por año y género inexistente
 	 * 
 	 * @param args Argumentos de la línea de comandos
 	 */
@@ -28,28 +25,21 @@ public class TestBiblioteca_1 {
 		List<Libro> libros = new ArrayList<> (List.of(
 				new Libro("1", "El Quijote", "Miguel de Cervantes", 5, 1605, "Novela"),
 				new Libro("2", "El murciélago", "Jo Nesbo", 1, 1997, "Misterio"),
-				new Libro("3", "Learn Java", "David Hoffman", 6, 2010, "Educación")));
+				new Libro("3", "Learn Java", "David Hoffman", 6, 2010, "Educación"),
+				new Libro("4", "Diez Negritos", "Agatha Christie", 7)));
 		Biblioteca biblio = new Biblioteca("BIBLIO");
 		for(Libro l :libros)
 			biblio.nuevolibro(l);
 		
-		libros.get(1).prestar();
-		System.out.println(biblio);
-		
-		libros.get(1).devolver();
-		System.out.println(biblio);
-		
-		libros.add(new Libro("4", "Con viento solano", "Ignacio Aldecoa", 1, 2007, "Novela"));
-		biblio.nuevolibro(libros.getLast());
 		System.out.println(biblio);
 		
 		List<Libro> libros_año = new ArrayList<> ();
-		libros_año = biblio.librosPosterioresA(1997);
+		libros_año = biblio.librosPosterioresA(2020);
 		for(Libro l: libros_año)
 			System.out.println(l);
 		
 		List<Libro> libros_genero = new ArrayList<> ();
-		libros_genero = biblio.librosPorGenero("Novela");
+		libros_genero = biblio.librosPorGenero("Ciencia ficción");
 		for(Libro l: libros_genero)
 			System.out.println(l);
 	}
